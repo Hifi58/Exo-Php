13062b7b0e774d8fb0740aed870721b7022a2dfe
Hifi58 <78985131+Hifi58@users.noreply.github.com>
2021-04-12 10:44:39 +0200

    Add files via upload

---
+++ exo9.php
@@ -0,0 +1,10 @@
+<?php
+//Créer un tableau classique comme en JS, qui contient les noms des personnes sur votre ilôt.
+
+$prenoms = array('Tony', 'Anthony', 'Julien', 'Tommy');
+
+for ($numero = 0; $numero < 4; $numero++)
+{
+    echo $prenoms[$numero] . '<br />'; 
+}
+?>
