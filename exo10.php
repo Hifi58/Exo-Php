8713651c710887b82900ac2ea50fc53c1515ca86
Hifi58 <78985131+Hifi58@users.noreply.github.com>
2021-04-12 10:51:10 +0200

    Add files via upload

---
+++ exo10.php
@@ -0,0 +1,11 @@
+<?php
+//Créer tableau associatif des personnes présentes sur votre îlot. Utilisez pour les clés "person1", "perosn2",...
+
+$prenoms = array(
+    'person1'=>'Tony', 
+    'person2'=>'Anthony',
+    'person3'=> 'Julien',
+    'person4'=> 'Tommy');
+
+    echo $prenoms ['person1'];
+    ?>
